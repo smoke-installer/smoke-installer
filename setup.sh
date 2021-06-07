6f8ce5653e92bbe0cab59cc34a8cd917c3f145b7
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-06-06 20:39:47 -0400

    Added .desktop file.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ sudo mkdir ~/SmokeInstaller
 sudo mkdir /etc/smoke_installer
 sudo cp -r bin/* /bin/
 sudo cp -r etc/* /etc/smoke_installer
+sudo cp -r Desktop/Smoke_Installer.desktop ~/Desktop/Smoke_Installer.desktop
